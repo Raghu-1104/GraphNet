d9f7c7bace13de01daef0570159b49c20f9497f0
Gaoh19 <aburahman918@gmail.com>
2024-03-27 12:02:22 +0530

    Implement Additional Features

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+const { PubSub } = require('graphql-subscriptions');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
 const { MONGODB } = require('./config.js');
+const typeDefs = require('./graphQL/typeDefs');
+const resolvers = require('./graphQL/resolvers');
 
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        createdAt:String!
-        username:String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(err){
-                throw new Error(err);
-            }
-        }
-    }
-}
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req })
+ typeDefs,
+ resolvers,
+ context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose.connect(MONGODB)
-    .then(() => {
-        console.log('MongoDB Connected')
-        return server.listen({port : 5000})
-    })
-
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    })
+ .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+ })
+ .then(res => {
+    console.log(`Server running at ${res.url}`);
+ })
+ .catch(err => console.error(err));
