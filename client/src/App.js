3878ce6f586519ce5280da0e633366c30098e99d
nishanth <nishanth23082004@gmail.com>
2024-05-01 15:34:11 +0530

    Merge remote changes into nishanth_branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
-import { BrowserRouter as Router, Route } from 'react-router-dom';
->>>>>>> ae54427f268f4f72357e74ddcd7e87933a381ebb
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
@@ -15,7 +11,6 @@ import Register from './pages/Register';
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <MenuBar />
       <Routes>
@@ -28,15 +23,3 @@ function App() {
 }
 
 export default App;
-=======
-      <Router>
-          <MenuBar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-      </Router>
-  );
-}
-
-export default App;
->>>>>>> ae54427f268f4f72357e74ddcd7e87933a381ebb
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { MenuMenu, MenuItem, Menu } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
 
 function MenuBar() {
   const [activeItem, setActiveItem] = useState('');
@@ -8,6 +9,7 @@ function MenuBar() {
 
     return (
         <Menu pointing secondary>
+          <Link to={"/"}>
           <MenuItem
             name='home'
             active={activeItem === 'home'}
@@ -18,18 +20,27 @@ function MenuBar() {
             active={activeItem === 'messages'}
             onClick={handleItemClick}
           />
+          </Link>
+          <Link to={"/register"}>
           <MenuMenu position='right'>
+            
             <MenuItem
               name='register'
               active={activeItem === 'register'}
               onClick={handleItemClick}
             />
           </MenuMenu>
+          </Link>
+          
+          
+          <Link to={"/login"}>
           <MenuItem
+          
             name='login'
             active={activeItem === 'login'}
             onClick={handleItemClick}
           />
+          </Link>
         </Menu>
     )
 }
