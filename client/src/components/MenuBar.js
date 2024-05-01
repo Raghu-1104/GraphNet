4143245808345a21d3f352bd437755f49077a1b1
Goah19 <aburahman918@gmail.com>
2024-05-01 19:06:46 +0530

    Implement live shifts

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,15 @@ import { MenuMenu, MenuItem, Menu } from 'semantic-ui-react'
 import { Link } from "react-router-dom";
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('');
+  const pathname = window.location.pathname;
+
+  const path = pathname === '/' ? 'home' : pathname.substring(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="teal">
           <Link to={"/"}>
 
           <MenuItem
