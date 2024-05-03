63a6983998327653f6e8a169396a396fe7efc3a0
Goah19 <aburahman918@gmail.com>
2024-05-03 08:36:52 +0530

    Resolve Dependencies

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,49 +1,56 @@
-import React, { useState } from 'react'
-import { MenuMenu, MenuItem, Menu } from 'semantic-ui-react'
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../context/auth';
 
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
 
-  const path = pathname === '/' ? 'home' : pathname.substring(1);
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
-        <Menu pointing secondary size="massive" color="teal">
-          <Link to={"/"}>
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
 
-          <MenuItem
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-          />
-          </Link>
-          <MenuItem
-            name='messages'
-            active={activeItem === 'messages'}
-            onClick={handleItemClick}
-          />
-          <MenuMenu position='right'>
-            <Link to={"/register"}>
-            <MenuItem
-              name='register'
-              active={activeItem === 'register'}
-              onClick={handleItemClick}
-            />
-            </Link>
-          </MenuMenu>
-          <Link to={"/login"}>
-          <MenuItem
-          
-            name='login'
-            active={activeItem === 'login'}
-            onClick={handleItemClick}
-          />
-          </Link>
-        </Menu>
-    )
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name="home"
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+
+  return menuBar;
 }
 
 export default MenuBar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import ApolloProvider from './ApolloProvider';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(ApolloProvider);
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
 
-reportWebVitals();;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
